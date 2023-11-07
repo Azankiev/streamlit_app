b82e3c0e0e37a34d6411b025e37f65bf7c6892a4
Azankiev <viniciusguilty@outlook.com>
2023-11-06 23:16:46 -0300

    Add files via upload

---
+++ streamlit app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+
+st.header("Minha dashboard")
+
+# Especifique o caminho do arquivo CSV
+caminho_arquivo = "C:/Users/vinic/Desktop/app stream/sales_data_sample.csv"
+
+# Abra o arquivo CSV e crie o DataFrame
+data_frame = pd.read_csv(caminho_arquivo, encoding='latin1')
+
+st.table(data_frame)
+
+
+colunas_grafico = ["QUANTITYORDERED", "SALES"]
+
+# Crie o gráfico utilizando as colunas selecionadas
+st.line_chart(data_frame[colunas_grafico])
+st.bar_chart(data_frame[colunas_grafico])
+
